4b6c6247b93eaae02ac3f388dfd2d941c42cfc0c
chris1203 <liuhuiling1203@gmail.com>
2017-02-16 14:43:14 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products
+  root 'products#index'
 end
