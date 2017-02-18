5f63b663ada7a6583aadbabf425e91e29dbdc5b6
chris1203 <liuhuiling1203@gmail.com>
2017-02-18 22:20:02 +0800

    landing page show by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   def index
     if params[:category].blank?
       @products = Product.all
+      @categories = Category.all
     else
       @category_id = Category.find_by(name: params[:category]).id
       @products = Product.where(:category_id => @category_id)
