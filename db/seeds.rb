04234890115896ac103a7360b4c6c303cb81292e
chris1203 <liuhuiling1203@gmail.com>
2017-02-18 18:17:03 +0800

    add seeds.rb detail

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,93 @@
 u = User.new(email: "aa@me.com", password: "111111", password_confirmation: "111111")
 u.is_admin=true
 u.save
+puts "1 admin user created"
+
+#create_categories
+categories_name = ["客厅", "卧室", "厨房","浴室", "其他"]
+
+create_categories = for i in 1..5 do
+Category.create!([name: categories_name[i - 1]])
+end
+
+puts"5 categories created"
+
+#create products of category1
+products_title = ["单人沙发", "吊灯","长沙发", "靠垫"]
+products_description = ["好1", "好2","好3","好4"]
+products_quantity = [10,30,100,10]
+products_price = [3000,200,100,500]
+
+create_products = for i in 1..4 do
+Product.create!([title: products_title[i - 1],
+description: products_description[i - 1],
+quantity: products_quantity[i - 1],
+price: products_price[i - 1],
+category_id: 1])
+end
+
+puts "4 products of category1 created."
+
+#create products of category2
+products_title = ["单人床", "双人床","落地灯", "衣柜"]
+products_description = ["好11", "好22","好33","好44"]
+products_quantity = [10,30,100,10]
+products_price = [3000,200,100,500]
+
+create_products = for i in 1..4 do
+Product.create!([title: products_title[i - 1],
+description: products_description[i - 1],
+quantity: products_quantity[i - 1],
+price: products_price[i - 1],
+category_id: 2])
+end
+
+puts "4 products of category2 created."
+
+#create products of category3
+products_title = ["汤锅", "餐盘","玻璃杯", "陶瓷杯"]
+products_description = ["好111", "好222","好333","好444"]
+products_quantity = [10,30,100,10]
+products_price = [3000,200,100,500]
+
+create_products = for i in 1..4 do
+Product.create!([title: products_title[i - 1],
+description: products_description[i - 1],
+quantity: products_quantity[i - 1],
+price: products_price[i - 1],
+category_id: 3])
+end
+
+puts "4 products of category3 created."
+
+#create products of category4
+products_title = ["喷头", "镜子","盥洗池", "按摩马桶"]
+products_description = ["好1111", "好2222","好3333","好4444"]
+products_quantity = [10,30,100,10]
+products_price = [3000,200,100,500]
+
+create_products = for i in 1..4 do
+Product.create!([title: products_title[i - 1],
+description: products_description[i - 1],
+quantity: products_quantity[i - 1],
+price: products_price[i - 1],
+category_id: 4])
+end
+
+puts "4 products of category4 created."
+
+#create products of category5
+products_title = ["窗帘", "桌布","杯垫", "书桌"]
+products_description = ["好11111", "好22222","好33333","好44444"]
+products_quantity = [10,30,100,10]
+products_price = [3000,200,100,500]
+
+create_products = for i in 1..4 do
+Product.create!([title: products_title[i - 1],
+description: products_description[i - 1],
+quantity: products_quantity[i - 1],
+price: products_price[i - 1],
+category_id: 5])
+end
+
+puts "4 products of category5 created."
